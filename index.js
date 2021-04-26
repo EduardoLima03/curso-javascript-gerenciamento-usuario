74403c12d4c7e86cf78550db5163e55ae8b39c7a
Carlos Lima <tec.carlos.lima@gmail.com>
2021-04-26 11:13:01 -0300

    variaves do form

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var nome = document.querySelector("#exampleInputName");
-
-nome.value = "Eduardo";
-
-nome.style.color = "blue";
+var name = document.querySelector("#exampleInputName");
+var genderM = document.querySelector('#form-user-create [name = gender]:checked');
+var birth = document.querySelector('#exampleInputBirth');
+var country = document.querySelector('#exampleInputCountry');
+var email = document.querySelector('#exampleInputEmail1');
+var password = document.querySelector('#exampleInputPassword1');
+var photo = document.querySelector('#exampleInputFile');
+var admin = document.querySelector('#exampleInputAdmin');

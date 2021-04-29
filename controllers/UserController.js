1a4855c8870fe1f3491ce67b90c69a4e8e736673
Carlos Lima <tec.carlos.lima@gmail.com>
2021-04-29 09:47:06 -0300

    Get,Set e Date

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -12,6 +12,10 @@ class UserController{
         this.formEl.addEventListener("submit", event =>{
     
             event.preventDefault();
+
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
             
             let values = this.getValues();
 
@@ -19,6 +23,9 @@ class UserController{
                 (context)=>{
                     values.photo = context;
                     this.addLine(values);
+
+                    this.formEl.reset();
+                    btn.disabled = false;
                 },
                 (e)=>{
                     console.log(e);
@@ -70,7 +77,7 @@ class UserController{
         });
 
 
-    }
+    }//fim getPhoto;
 
     getValues(){
 
@@ -106,7 +113,7 @@ class UserController{
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${(dataUser.admin)?'Sim':'Não'}</td>
-            <td>${dataUser.birth}</td>
+            <td>${dataUser.register}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
